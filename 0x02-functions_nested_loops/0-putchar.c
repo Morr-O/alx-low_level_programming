c85d1566412c79b4403956d80605cfc0f39c9cfd
Morr-O <morogara5@gmail.com>
2022-03-15 11:13:53 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ *message is putchar
+ */
+int main(void)
+{
+   int str[]={95, 112, 117, 99, 104, 97, 114};
+   int count, sz;
+   sz = sizeof(str)/ sizeof(int);
+   for (count=o; count<sz; count++)
+   {
+      _putchar(str[count]);
+   }
+   _putchar('\n');
+   return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This reasdme file for another directory
