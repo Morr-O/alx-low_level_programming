c24d47c24377cd7367880cc6fa38619c8e405350
Morr-O <morogara5@gmail.com>
2022-03-15 12:05:36 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,11 @@
-#include <putchar.h>
+#include <stdio.h>
 /**
- *message is putchar
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-   int str[]={95, 112, 117, 99, 104, 97, 114};
-   int count, sz;
-   sz = sizeof(str)/ sizeof(int);
-   for (count=o; count<sz; count++)
-   {
-      _putchar(str[count]);
-   }
-   _putchar('\n');
-   return (0);
+printf("_putchar\n");
+return (0);
 }
