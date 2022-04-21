07ea7e57c4fb7084333642545d4e66c8be7c5ce5
Morr-O <morogara5@gmail.com>
2022-04-21 05:50:45 -0700

    Printing values of a linkedlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,17 +5,20 @@
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t num;
-	num=0;
-	while (h !=NULL) {
-		if (h->str==NULL)
-			printf("[%d] %s\n", 0,"(nil)");
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("%d %s\n", h-> data);
-			h =h->next;
-			num++;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
 	}
-	return num;
+	return (nelem);
 }
